dbcb54534e43cc09e4311dabaa1f1c8b811ea2b5
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-09-07 00:31:45 +0300

    задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите число строк массива m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matr = new int [m,n];
+
+void PrintArray (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Console.Write($"{matr[i, j]} ");
+        }
+Console.WriteLine();
+    }
+}
+
+void FillRndArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Random rand = new Random();
+        matr[i, j] = rand.Next(1, 10);
+        }
+    }   
+}
+
+int mamimalArray(int[,] matr, int i)
+{
+  int Summ = matr[i,0];
+  for (int j = 1; j < matr.GetLength(1); j++)
+  {
+    Summ += matr[i,j];
+  }
+  return Summ;
+}
+
+
+int minSumm = 0;
+int Summ = mamimalArray(matr, 0);
+for (int i = 1; i < matr.GetLength(0); i++)
+{
+  int tempSumm = mamimalArray(matr, i);
+  if (Summ > tempSumm)
+  {
+    Summ = tempSumm;
+    minSumm = i;
+  }
+}
+
+
+
+FillRndArray(matr);
+PrintArray(matr);
+Console.WriteLine();
+Console.WriteLine($"Строка с наименьшей суммой элементов -> {minSumm}");
