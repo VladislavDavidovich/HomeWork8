d2f79e07392b1e142957435efb5e699da406f1ce
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-09-08 00:47:37 +0300

    изменение с перестановкой методов

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,41 +6,6 @@ Console.WriteLine("Введите число столбцов массива n:"
 int n = Convert.ToInt32(Console.ReadLine());
 int[,] matr = new int [m,n];
 
-void PrintArray (int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-        Console.Write($"{matr[i, j]} ");
-        }
-Console.WriteLine();
-    }
-}
-
-void FillRndArray(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-        Random rand = new Random();
-        matr[i, j] = rand.Next(1, 10);
-        }
-    }   
-}
-
-int mamimalArray(int[,] matr, int i)
-{
-  int Summ = matr[i,0];
-  for (int j = 1; j < matr.GetLength(1); j++)
-  {
-    Summ += matr[i,j];
-  }
-  return Summ;
-}
-
-
 int minSumm = 0;
 int Summ = mamimalArray(matr, 0);
 for (int i = 1; i < matr.GetLength(0); i++)
@@ -54,8 +19,46 @@ for (int i = 1; i < matr.GetLength(0); i++)
 }
 
 
+int mamimalArray(int[,] matr, int i)
+{
+  int Summ = matr[i,0];
+  for (int j = 1; j < matr.GetLength(1); j++)
+  {
+    Summ += matr[i,j];
+  }
+  return Summ;
+}
+
+
+
+void FillRndArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Random rand = new Random();
+        matr[i, j] = rand.Next(1, 10);
+        }
+    }   
+}
+
+
+void PrintArray (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Console.Write($"{matr[i, j]} ");
+        }
+Console.WriteLine();
+    }
+}
+
 
 FillRndArray(matr);
 PrintArray(matr);
 Console.WriteLine();
 Console.WriteLine($"Строка с наименьшей суммой элементов -> {minSumm}");
+
